bac203147debe58fad5886c2e047030c6454b9bf
JCash <mwesterdahl76@gmail.com>
2025-10-15 23:33:37 +0200

    fix for including multiple times

--- scripts/cmake/sdk.cmake
+++ scripts/cmake/sdk.cmake
@@ -1,7 +1,7 @@
 if(DEFINED DEFOLD_SDK_CMAKE_INCLUDED)
     return()
 endif()
-set(DEFOLD_SDK_CMAKE_INCLUDED ON CACHE INTERNAL "sdk.cmake include guard")
+set(DEFOLD_SDK_CMAKE_INCLUDED ON)
 
 if(DEFINED DEFOLD_SDK_DETECTED)
     # Avoid re-running detection when sdk.cmake is included multiple times
